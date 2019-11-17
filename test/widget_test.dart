d721c3b3296a930b4719580194001fd0ce6c3e4f
zois avgerinos <zois_avger@live.com>
2019-11-17 02:36:48 +0200

    App is running

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:pblcwallet/service/address_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pblcwallet/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('should return private key from mnemonic', () {
+    final addressService = AddressService(null);
+    final privateKey = addressService.getPrivateKey(
+        "loan absorb orange crouch mixed position sweet law ghost habit upgrade toss");
+    expect(privateKey,
+        "926675e8e39e176d86fb55ea01c577325b6e385fa147bd7ceb72c7f9949a7a96");
   });
 }
